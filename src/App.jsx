8b2a6ccd0bcd77799c29ed6e229a080b41947ba8
abhay2sing <abhaysingh480shiv@gmail.com>
2025-05-06 23:08:37 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Todo from './componentss/todo'
+import Todo from './componentss/Todo'
 
 
 function App() {
